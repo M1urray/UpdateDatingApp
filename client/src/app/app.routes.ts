301090f6730cddd25ed2ebcf0360dfae88bfb455
M1urray <rknjonjo@gmail.com>
2025-01-21 10:14:59 +0300

    End of Section 7

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { authGuard } from './_guards/auth.guard';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServererrorComponent } from './errors/servererror/servererror.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -13,11 +16,14 @@ export const routes: Routes = [
         runGuardsAndResolvers: 'always',
         canActivate: [authGuard],
         children: [
-            { path: 'members', component: MemberListComponent},
+            { path: 'members', component: MemberListComponent },
             { path: 'members/:id', component: MemberDetailComponent },
             { path: 'lists', component: ListsComponent },
             { path: 'messages', component: MessagesComponent },
         ]
     },
+    { path: 'errors', component: TestErrorsComponent },
+    { path: 'not-found', component: NotFoundComponent },
+    { path: 'server-error', component: ServererrorComponent },
     { path: '**', component: HomeComponent, pathMatch: 'full' }
 ];
