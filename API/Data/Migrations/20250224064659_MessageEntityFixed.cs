f3bfb78ec2cd234a72d9574519e807ba411f5bea
Robert Kamau Njonjo <rknjonjo@gmail.com>
2025-02-28 10:27:01 +0300

    End of section 15

---
+++ API/Data/Migrations/20250224064659_MessageEntityFixed.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MessageEntityFixed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DatRead",
+                table: "Messages",
+                newName: "DateRead");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateRead",
+                table: "Messages",
+                newName: "DatRead");
+        }
+    }
+}
